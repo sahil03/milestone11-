4b83765d0664838bf546db1cedc8880f8f036f26
sahil03 <gupta.sahil6116@gmail.com>
2012-09-06 17:37:58 -0700

    Update SE/src/HelloWorld.java

--- SE/src/HelloWorld.java
+++ SE/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	
 		System.out.println("Hello World");
 		System.out.println("Hello Sahil");
-		System.out.println("Hello Rishi");
+		System.out.println("Hello Rishi"); //change
 	}
 
 }
